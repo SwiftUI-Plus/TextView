0b0d1e0ef2013747006148fb691ed30639431125
Shaps Benkau <shapsuk@me.com>
2021-08-30 01:32:59 +0100

    Added header docs

--- Sources/TextView/TextView.swift
+++ Sources/TextView/TextView.swift
@@ -1,30 +1,6 @@
 import SwiftUI
 
-struct RoundedTextView: View {
-    @State private var text: String = ""
-
-    var body: some View {
-        GeometryReader { _ in
-            TextView($text)
-                .placeholder("Enter some text")
-                .padding(10)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(lineWidth: 1)
-                        .foregroundColor(Color(.placeholderText))
-                )
-                .padding()
-        }
-        .background(Color(.systemBackground).edgesIgnoringSafeArea(.all))
-    }
-}
-
-struct TextView_Previews: PreviewProvider {
-    static var previews: some View {
-        RoundedTextView()
-    }
-}
-
+/// A SwiftUI TextView implementation that supports both scrolling and auto-sizing layouts
 public struct TextView: View {
 
     @Environment(\.layoutDirection) private var layoutDirection
@@ -61,6 +37,12 @@ public struct TextView: View {
         }
     }
 
+    /// Makes a new TextView with the specified configuration
+    /// - Parameters:
+    ///   - text: A binding to the text
+    ///   - shouldEditInRange: A closure that's called before an edit it applied, allowing the consumer to prevent the change
+    ///   - onEditingChanged: A closure that's called after an edit has been applied
+    ///   - onCommit: If this is provided, the field will automatically lose focus when the return key is pressed
     public init(_ text: Binding<String>,
          shouldEditInRange: ((Range<String.Index>, String) -> Bool)? = nil,
          onEditingChanged: (() -> Void)? = nil,
@@ -116,10 +98,20 @@ public struct TextView: View {
 
 public extension TextView {
 
+    /// Specify a placeholder text
+    /// - Parameter placeholder: The placeholder text
     func placeholder(_ placeholder: String) -> TextView {
         self.placeholder(placeholder) { $0 }
     }
 
+    /// Specify a placeholder with the specified configuration
+    ///
+    /// Example:
+    ///
+    ///     TextView($text)
+    ///         .placeholder("placeholder") { view in
+    ///             view.foregroundColor(.red)
+    ///         }
     func placeholder<V: View>(_ placeholder: String, _ configure: (Text) -> V) -> TextView {
         var view = self
         let text = Text(placeholder)
@@ -127,52 +119,69 @@ public extension TextView {
         return view
     }
 
+    /// Specify a custom placeholder view
     func placeholder<V: View>(_ placeholder: V) -> TextView {
         var view = self
         view.placeholderView = AnyView(placeholder)
         return view
     }
 
+    /// Enables auto detection for the specified types
+    /// - Parameter types: The types to detect
     func autoDetectDataTypes(_ types: UIDataDetectorTypes) -> TextView {
         var view = self
         view.autoDetectionTypes = types
         return view
     }
 
+    /// Specify the foreground color for the text
+    /// - Parameter color: The foreground color
     func foregroundColor(_ color: UIColor) -> TextView {
         var view = self
         view.foregroundColor = color
         return view
     }
 
+    /// Specifies the capitalization style to apply to the text
+    /// - Parameter style: The capitalization style
     func autocapitalization(_ style: UITextAutocapitalizationType) -> TextView {
         var view = self
         view.autocapitalization = style
         return view
     }
 
+    /// Specifies the alignment of multi-line text
+    /// - Parameter alignment: The text alignment
     func multilineTextAlignment(_ alignment: TextAlignment) -> TextView {
         var view = self
         view.multilineTextAlignment = alignment
         return view
     }
 
+    /// Specifies the font to apply to the text
+    /// - Parameter font: The font to apply
     func font(_ font: UIFont) -> TextView {
         var view = self
         view.font = font
         return view
     }
 
+    /// Specifies the font weight to apply to the text
+    /// - Parameter weight: The font weight to apply
     func fontWeight(_ weight: UIFont.Weight) -> TextView {
         font(font.weight(weight))
     }
 
+    /// Specifies if the field should clear its content when editing begins
+    /// - Parameter value: If true, the field will be cleared when it receives focus
     func clearOnInsertion(_ value: Bool) -> TextView {
         var view = self
         view.clearsOnInsertion = value
         return view
     }
 
+    /// Disables auto-correct
+    /// - Parameter disable: If true, autocorrection will be disabled
     func disableAutocorrection(_ disable: Bool?) -> TextView {
         var view = self
         if let disable = disable {
@@ -183,36 +192,48 @@ public extension TextView {
         return view
     }
 
+    /// Specifies whether the text can be edited
+    /// - Parameter isEditable: If true, the text can be edited via the user's keyboard
     func isEditable(_ isEditable: Bool) -> TextView {
         var view = self
         view.isEditable = isEditable
         return view
     }
 
+    /// Specifies whether the text can be selected
+    /// - Parameter isSelectable: If true, the text can be selected
     func isSelectable(_ isSelectable: Bool) -> TextView {
         var view = self
         view.isSelectable = isSelectable
         return view
     }
 
+    /// Specifies whether the field can be scrolled. If true, auto-sizing will be disabled
+    /// - Parameter isScrollingEnabled: If true, scrolling will be enabled
     func enableScrolling(_ isScrollingEnabled: Bool) -> TextView {
         var view = self
         view.isScrollingEnabled = isScrollingEnabled
         return view
     }
 
+    /// Specifies the type of return key to be shown during editing, for the device keyboard
+    /// - Parameter style: The return key style
     func returnKey(_ style: UIReturnKeyType?) -> TextView {
         var view = self
         view.returnKeyType = style
         return view
     }
 
+    /// Specifies whether the return key should auto enable/disable based on the current text
+    /// - Parameter value: If true, when the text is empty the return key will be disabled
     func automaticallyEnablesReturn(_ value: Bool?) -> TextView {
         var view = self
         view.enablesReturnKeyAutomatically = value
         return view
     }
 
+    /// Specifies the truncation mode for this field
+    /// - Parameter mode: The truncation mode
     func truncationMode(_ mode: Text.TruncationMode) -> TextView {
         var view = self
         switch mode {
@@ -432,4 +453,29 @@ private final class UIKitTextView: UITextView {
         resignFirstResponder()
     }
 
+}
+
+struct RoundedTextView: View {
+    @State private var text: String = ""
+
+    var body: some View {
+        GeometryReader { _ in
+            TextView($text)
+                .placeholder("Enter some text")
+                .padding(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(lineWidth: 1)
+                        .foregroundColor(Color(.placeholderText))
+                )
+                .padding()
+        }
+        .background(Color(.systemBackground).edgesIgnoringSafeArea(.all))
+    }
+}
+
+struct TextView_Previews: PreviewProvider {
+    static var previews: some View {
+        RoundedTextView()
+    }
 }
